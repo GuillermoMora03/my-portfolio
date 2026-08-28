ce70c0aedd5ff8bbeb05b8575e259c6421313d11
Guillermo Mora <guillermo_moram@outlook.es>
2026-08-28 11:41:18 -0600

    feat: agregar archivo IPersonalInfoService y PersonalInfoServiceImpl en la carpeta service

---
+++ src/main/java/com/porfolio/my_portfolio_backend/service/IPersonalInfoService.java
@@ -0,0 +1,21 @@
+package com.porfolio.my_portfolio_backend.service;
+
+import com.porfolio.my_portfolio_backend.model.PersonalInfo;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface IPersonalInfoService {
+
+    // Será para guardar la información y tambien se contempla el update dentro del save
+    PersonalInfo save(PersonalInfo personalInfo);
+
+    // Será para buscar por ID
+    Optional<PersonalInfo> findById(Long id);
+
+    // Será para devolver a todos
+    List<PersonalInfo> findAll();
+
+    // Será para borrar por ID
+    void deleteById(Long id);
+}
