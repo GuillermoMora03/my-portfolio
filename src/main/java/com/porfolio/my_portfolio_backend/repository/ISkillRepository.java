7a51ae034ab174a763f66caa3caab572f32fd2e3
Guillermo Mora <guillermo_moram@outlook.es>
2026-08-28 12:27:43 -0600

    feat: agregar archivos para el repository y su implementacion en la carpeta repository de Skill

---
+++ src/main/java/com/porfolio/my_portfolio_backend/repository/ISkillRepository.java
@@ -0,0 +1,24 @@
+package com.porfolio.my_portfolio_backend.repository;
+
+import com.porfolio.my_portfolio_backend.model.Skill;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface ISkillRepository {
+
+    // Será para guardar la información y tambien se contempla el update dentro del save
+    Skill save(Skill skill);
+
+    // Será para buscar por ID
+    Optional<Skill> findById(Long id);
+
+    // Será para devolver a todos
+    List<Skill> findAll();
+
+    // Será para borrar por ID
+    void deleteById(Long id);
+
+    List<Skill> findByPersonalInfoId(Long personalInfoId);
+
+}
