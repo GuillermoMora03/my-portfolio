b23596ed63023066aba1b9c56fea693a2c90ad55
Guillermo Mora <guillermo_moram@outlook.es>
2026-08-28 18:49:21 -0600

    feat: agregar archivos para la carpeta repository, el servicio para educacion y la implementacion del mismo

---
+++ src/main/java/com/porfolio/my_portfolio_backend/repository/IEducationRepository.java
@@ -0,0 +1,24 @@
+package com.porfolio.my_portfolio_backend.repository;
+
+import com.porfolio.my_portfolio_backend.model.Education;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface IEducationRepository {
+
+    // Será para guardar la información y tambien se contempla el update dentro del save
+    Education save(Education education);
+
+    // Será para buscar por ID
+    Optional<Education> findById(Long id);
+
+    // Será para devolver a todos
+    List<Education> findAll();
+
+    // Será para borrar por ID
+    void deleteById(Long id);
+
+    List<Education> findByPersonalInfoId(Long personalInfoId);
+
+}
