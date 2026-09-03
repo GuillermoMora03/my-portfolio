1317e6e54321b4011fb26f3f092c9bd97ff48874
Guillermo Mora <guillermo_moram@outlook.es>
2026-09-03 17:43:57 -0600

    feat: agregar validaciones con validation

--- src/main/java/com/porfolio/my_portfolio_backend/model/Experience.java
+++ src/main/java/com/porfolio/my_portfolio_backend/model/Experience.java
@@ -1,5 +1,8 @@
 package com.porfolio.my_portfolio_backend.model;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PastOrPresent;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -11,10 +14,22 @@ import java.time.LocalDate;
 @NoArgsConstructor
 public class Experience {
     private Long id;
+
+    @NotBlank(message = "El nombre del puesto no puede estar vacío")
     private String jobTitle; // Ej: "Full Stack Developer"
+
+    @NotBlank(message = "El nombre de la compañia no puede estar vacío")
     private String companyName;
+
+    @NotNull(message = "La fecha de inicio no puede ser nula")
+    @PastOrPresent(message = "La fecha de inicio no puede ser futura ")
     private LocalDate startDate;
+
+    @PastOrPresent(message = "La fecha de fin no puede ser futura")
     private LocalDate endDate; // Puede ser null si es el puesto actual
+
+    @NotBlank(message = "La descripción no puede estar vacía")
     private String description; // Responsabilidades y logros
+
     private Long personalInfoId; // Clave foránea a PersonalInfo
 }
